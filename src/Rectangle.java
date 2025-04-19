940f73cea36d2c98308c8e90f5b5c02c480ab3da
Aaron Zhang <aaron@weikai.net>
2025-04-19 19:57:16 -0400

    Rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+public class Rectangle extends JPanel {
+    public Rectangle(double width, double height) {
+        
+    }
+    
+    public Rectangle() {
+
+    }
+
+
+}
